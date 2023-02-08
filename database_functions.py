fb28431fb17dd1b0c737432f369e339e634b6aab
JohnCunan <76254034+JohnCunan@users.noreply.github.com>
2023-02-08 11:00:34 +0800

    final_changes

--- database_functions.py
+++ database_functions.py
@@ -3,8 +3,9 @@ import pickle
 import pyodbc
 from datetime import date
 from datetime import datetime
+from datetime import timedelta
 from clock import get_time, get_date
-from time_diff import get_minutes_late
+from time_diff import get_minutes_late, convert
 from pop_ups import timeout_error_message, employee_inactive_message
 
 with open('Persistence Files/db_name.pickle', 'rb') as f:
@@ -33,7 +34,7 @@ def N_time_in(connection, detected_employee_id):
     if status == "Active":
         cursor.execute(
             "IF NOT EXISTS (SELECT EmployeeID, Date FROM AttendanceRecord "
-            "WHERE EmployeeID='" + detected_employee_id + "'" + " AND Date='" + get_date() + "')"
+            "WHERE EmployeeID='" + str(detected_employee_id) + "'" + " AND Date='" + get_date() + "')"
             "BEGIN "
             "INSERT INTO AttendanceRecord(AttendanceID, EmployeeID, TimeIn, Date, PositionID) values(?, ?, ?, ?, ?)"
             "END",
@@ -51,35 +52,60 @@ def N_time_out(connection, detected_employee):
     )
     connection.commit()
 
-############################################
-#     ADD ACCUMULATED DAY OFF SCHEDULE     #
-############################################
 
+def time_out_no_in(connection, detected_employee_id):
+    attendance_id = str(date.today()).replace('-', '') + str(detected_employee_id)
+    status = check_employee_status(detected_employee_id)
 
-def N_add_accumulated_day_off(id):
-    if datetime.today().isoweekday() == 7:
+    if status == "Active":
         cursor.execute(
-            "UPDATE EmployeeInfo SET AccumulatedDayOffs = AccumulatedDayOffs + 1 WHERE EmployeeID='" + str(id) + "'"
+            "IF NOT EXISTS (SELECT EmployeeID, Date FROM AttendanceRecord "
+            "WHERE EmployeeID='" + str(detected_employee_id) + "'" + " AND Date='" + get_date() + "')"
+            "BEGIN "
+            "INSERT INTO AttendanceRecord(AttendanceID, EmployeeID, TimeOut, Date, PositionID) values(?, ?, ?, ?, ?)"
+            "END",
+            (attendance_id, detected_employee_id, get_time(), get_date(), N_get_position_id(detected_employee_id))
         )
         connection.commit()
-
-
+    else:
+        employee_inactive_message()
 ############################################
 #     INSERT TIME VALUES INTO DATABASE     #
 ############################################
 
 
+def time_in_range_break_time_in(start, end, x):
+    """Return true if x is in the range [start, end]"""
+    if start is None:
+        return
+    elif end is None:
+        return
+    else:
+        if start <= end:
+            return start <= x <= end
+        else:
+            return start <= x or x <= end
+
+
 def N_minutes_late(connection, detected_employee, minutes):
     cursor.execute(
         "UPDATE AttendanceRecord SET Late_Minutes=" + str(minutes) + " " +
-        "WHERE EmployeeID='" + detected_employee + "' AND Date='" + get_date() + "'"
+        "WHERE EmployeeID='" + detected_employee + "' AND Date='" + get_date() + "' "
+        "AND Late_Minutes IS NULL"
     )
     connection.commit()
 
 
 def insert_hours_and_minutes(connection, detected_employee, total_time, break_period_point):
+
     if '-' in str(total_time):
         total_time = str(total_time)[8:]
+        total_time = '00:00:00'
+
+    # Employee time out before timing in error catch
+    if total_time is None:
+        # Error is already caught on recognize.py line 322
+        return
 
     # Split 12-hour time format to h m s
     (h, m, s) = str(total_time).split(':')
@@ -89,6 +115,101 @@ def insert_hours_and_minutes(connection, detected_employee, total_time, break_pe
     # Break subtractor
     if break_period_point:
         h = int(h) - 1
+
+    # If timing in during break
+    break_time = datetime.strptime(get_employee_break(detected_employee), "%I:%M:%S %p")
+    break_end_24 = break_time + timedelta(hours=1)
+    break_end_12 = datetime.strftime(break_end_24, "%I:%M:%S %p")
+
+    date = get_date()
+
+    if time_in_range_break_time_in(get_employee_break(detected_employee), break_end_12, N_get_employee_time_in(detected_employee, get_date())):
+        time_1 = datetime.strptime(convert(str(break_end_12)), "%H:%M:%S")
+        time_2 = datetime.strptime(convert(N_get_employee_time_in(detected_employee, date))[:8], "%H:%M:%S")
+
+        mins_break_in = time_1 - time_2
+        (bh, bm, bs) = str(mins_break_in).split(':')
+        h = h + 1
+        m = int(m) - int(bm)
+
+    # If timing out during break
+    if time_in_range_break_time_in(get_employee_break(detected_employee), break_end_12, N_get_employee_time_out(detected_employee, get_date())):
+        time_1_out = datetime.strptime(convert(get_employee_break(detected_employee))[:8], "%H:%M:%S")
+        time_2_out = datetime.strptime(convert(N_get_employee_time_out(detected_employee, date))[:8], "%H:%M:%S")
+
+        mins_break_in = time_2_out - time_1_out
+        (bh, bm, bs) = str(mins_break_in).split(':')
+        h = h + 1
+        m = int(m) - int(bm)
+
+    # Grace Period Adder
+    if mins_late <= 10:
+        m = int(m) + int(mins_late)
+
+        if m >= 60:
+            h = h + 1
+            m = m - 60
+
+    # 8 Hour limiter
+    if int(h) > 8:
+        h = 8
+        m = 0
+
+    cursor.execute(
+        "UPDATE AttendanceRecord SET Hours=" + str(h) + ", Minutes=" + str(m) + " "
+        "WHERE EmployeeID='" + detected_employee + "' "
+        "AND Date='" + get_date() + "' AND Hours IS NULL AND Minutes IS NULL"
+    )
+    connection.commit()
+
+
+def insert_hours_and_minutes_single_sched(connection, detected_employee, total_time, break_period_point):
+    if '-' in str(total_time):
+        total_time = str(total_time)[8:]
+        total_time = '00:00:00'
+
+    # Split 12-hour time format to h m s
+
+    # Employee time out before timing in error catch
+    if total_time is None:
+        # Error is already caught on recognize.py line 322
+        return
+
+    (h, m, s) = str(total_time).split(':')
+
+    mins_late = int(get_minutes_late(N_get_single_sched_start(detected_employee), N_get_employee_time_in(detected_employee, get_date())))
+
+    # Break subtractor
+    if break_period_point:
+        h = int(h) - 1
+
+    # If timing in during break
+    break_time = datetime.strptime(get_single_sched_break(detected_employee), "%I:%M:%S %p")
+    break_end_24 = break_time + timedelta(hours=1)
+    break_end_12 = datetime.strftime(break_end_24, "%I:%M:%S %p")
+
+    date = get_date()
+
+    if time_in_range_break_time_in(get_single_sched_break(detected_employee), break_end_12,
+                                   N_get_employee_time_in(detected_employee, get_date())):
+        time_1 = datetime.strptime(convert(str(break_end_12))[:8], "%H:%M:%S")
+        time_2 = datetime.strptime(convert(N_get_employee_time_in(detected_employee, date))[:8], "%H:%M:%S")
+
+        mins_break_in = time_1 - time_2
+        (bh, bm, bs) = str(mins_break_in).split(':')
+        # h = h + 1
+        m = int(m) - int(bm)
+
+    # If timing out during break
+    if time_in_range_break_time_in(get_single_sched_break(detected_employee), break_end_12,
+                                   N_get_employee_time_out(detected_employee, get_date())):
+        time_1_out = datetime.strptime(convert(get_single_sched_break(detected_employee))[:8], "%H:%M:%S")
+        time_2_out = datetime.strptime(convert(N_get_employee_time_out(detected_employee, date))[:8], "%H:%M:%S")
+
+        mins_break_in = time_2_out - time_1_out
+        (bh, bm, bs) = str(mins_break_in).split(':')
+        h = h + 1
+        m = int(m) - int(bm)
 
     # Grace Period Adder
     if mins_late <= 10:
@@ -179,6 +300,17 @@ def N_check_and_insert_holiday(id, hours, break_period_point):
     size = len(str(get_date()))
     current_date = str(get_date())[:size - 6]
 
+    cursor.execute("SELECT Type_ FROM Holidays WHERE From_ ='" + current_date + "'")
+
+    for row in cursor:
+        time = str(row)
+        holiday = re.sub("[(',)]", '', time).rstrip()
+
+    # Time out before time in error catcher
+    if hours is None:
+        # Error already caught at recognize.py line 322
+        return
+
     if '-' in str(hours):
         hours = str(hours)[8:]
 
@@ -187,11 +319,24 @@ def N_check_and_insert_holiday(id, hours, break_period_point):
     if break_period_point:
         h = int(h) - 1
 
-    cursor.execute("SELECT Type_ FROM Holidays WHERE From_ ='" + current_date + "'")
+    # Grace Period for Single Sched and Normal Sched
+    if N_get_single_sched_date(id, get_date()) == get_date():
+        mins_late = int(get_minutes_late(N_get_single_sched_start(id), N_get_employee_time_in(id, get_date())))
+    else:
+        mins_late = int(get_minutes_late(get_employee_schedule_start(id), N_get_employee_time_in(id, get_date())))
 
-    for row in cursor:
-        time = str(row)
-        holiday = re.sub("[(',)]", '', time).rstrip()
+    # Grace Period Adder
+    if mins_late <= 10:
+        m = int(m) + int(mins_late)
+
+        if m >= 60:
+            h = h + 1
+            m = m - 60
+
+    # 8 Hour limiter
+    if int(h) > 8:
+        h = 8
+        m = 0
 
     if holiday == 'Regular Holiday':
         insert_reg_holiday(connection, detected_employee=id, hours=h, minutes=m)
@@ -212,6 +357,70 @@ def N_current_holiday():
         time = str(row)
         return re.sub("[(',)]", '', time).rstrip()
 
+
+####################################################
+#    INSERT REST DAY TIME VALUES INTO DATABASE     #
+####################################################
+
+def insert_rest_day(connection, detected_employee, hours, minutes):
+    cursor.execute(
+        "UPDATE AttendanceRecord SET RestDay=1, "
+        "RD_Hours=" + str(hours) + ", RD_Minutes=" + str(minutes) +
+        "WHERE EmployeeID='" + detected_employee + "' AND Date='" + get_date() + "'"
+    )
+    connection.commit()
+
+
+def insert_none_rest_day(connection, detected_employee):
+    cursor.execute(
+        "UPDATE AttendanceRecord SET RestDay=0, "
+        "RD_Hours=0, RD_Minutes=0" +
+        "WHERE EmployeeID='" + detected_employee + "' AND Date='" + get_date() + "'"
+    )
+    connection.commit()
+
+
+def check_and_insert_rest_day(id, hours, break_period_point):
+    # Time out before time in error catcher
+    if hours is None:
+        # Error already caught at recognize.py line 322
+        return
+
+    if '-' in str(hours):
+        hours = str(hours)[8:]
+        hours = '00:00:00'
+
+    (h, m, s) = str(hours).split(':')
+
+    if break_period_point:
+        h = int(h) - 1
+
+    # Grace Period for Single Sched and Normal Sched
+    if N_get_single_sched_date(id, get_date()) == get_date():
+        mins_late = int(get_minutes_late(N_get_single_sched_start(id), N_get_employee_time_in(id, get_date())))
+    else:
+        mins_late = int(get_minutes_late(get_employee_schedule_start(id), N_get_employee_time_in(id, get_date())))
+
+    # Grace Period Adder
+    if mins_late <= 10:
+        m = int(m) + int(mins_late)
+
+        if m >= 60:
+            h = h + 1
+            m = m - 60
+
+    # 8 Hour limiter
+    if int(h) > 8:
+        h = 8
+        m = 0
+
+    rest_day = get_employee_rest_day(id)
+
+    # If false then it is rest day
+    if rest_day == 'False':
+        insert_rest_day(connection, detected_employee=id, hours=h, minutes=m)
+    else:
+        insert_none_rest_day(connection, detected_employee=id)
 
 #####################################################
 #  GET ATTENDANCE RECORD INFORMATION FROM DATABASE  #
@@ -243,12 +452,12 @@ def get_attendance_hours(id, date):
 
 
 ############################################
-#               CHECK LEAVE                #
+#             CHECK LEAVE & OT             #
 ############################################
 
 def N_check_employee_leave(id, date):
     cursor.execute("SELECT Date FROM LeavePay "
-                   "WHERE EmployeeID='" + id + "' AND Date='" + date + "'")
+                   "WHERE EmployeeID='" + str(id) + "' AND Date='" + date + "'")
     for row in cursor:
         checked_date = str(row)
         checked_date = re.sub("[(',)]", '', checked_date).rstrip()
@@ -260,6 +469,26 @@ def N_check_employee_leave(id, date):
             # Employee is on a Leave
             return True
 
+
+def check_employee_overtime(id, date):
+
+    if date is None:
+        # Time out before time in error catcher
+        timeout_error_message()
+        return
+
+    cursor.execute("SELECT Date FROM OvertimeDates "
+                   "WHERE EmployeeID='" + id + "' AND Date='" + date + "'")
+    for row in cursor:
+        checked_date = str(row)
+        checked_date = re.sub("[(',)]", '', checked_date).rstrip()
+
+        if checked_date is None:
+            # Employee is not on a leave
+            return False
+        else:
+            # Employee is on a Leave
+            return True
 
 ############################################
 #             CHECK WEEKDAYS               #
@@ -323,6 +552,14 @@ def N_get_single_sched_end(id):
 ############################################
 #  GET EMPLOYEE INFORMATION FROM DATABASE  #
 ############################################
+
+
+def get_employee_id(name):
+    cursor.execute("SELECT EmployeeID FROM EmployeeInfo "
+                   "WHERE EmployeeFullName='" + str(name) + "'")
+    for row in cursor:
+        name = str(row)
+        return re.sub("[(',)]", '', name).rstrip()
 
 
 def N_get_employee_name(id):
@@ -396,3 +633,76 @@ def N_check_allowed_ot(id):
         time = str(row)
         return re.sub("[(',)]", '', time).rstrip()
 
+
+def get_employee_break(id):
+    cursor.execute("SELECT BreakPeriod FROM EmployeeSchedule "
+                   "WHERE EmployeeID=" + str(id) + "")
+    for row in cursor:
+        time = str(row)
+        return re.sub("[(',)]", '', time).rstrip()
+
+
+def get_employee_status(id):
+    cursor.execute("SELECT Status FROM EmployeeInfo "
+                   "WHERE EmployeeID='" + str(id) + "'")
+    for row in cursor:
+        time = str(row)
+        return re.sub("[(',)]", '', time).rstrip()
+
+
+def get_employee_list():
+    e_list = []
+
+    cursor.execute("SELECT EmployeeID FROM EmployeeInfo WHERE Status='Active'")
+    for row in cursor:
+        emp = str(row)
+        e_list.append(re.sub("[(',)]", '', emp).rstrip())
+
+    return e_list
+
+
+def get_single_sched_break(id):
+    cursor.execute("SELECT BreakPeriod FROM SingleSchedule "
+                   "WHERE EmployeeID='" + str(id) + "'")
+    for row in cursor:
+        time = str(row)
+        return re.sub("[(',)]", '', time).rstrip()
+
+
+def get_employee_list_id():
+    e_list = []
+
+    cursor.execute("SELECT EmployeeID, EmployeeFullName FROM EmployeeInfo WHERE Status='Active'"
+                   " ORDER BY EmployeeFullName")
+
+    for row in cursor:
+        emp = str(row)
+        e_list.append(re.sub("[(')]", '', emp).rstrip().replace(" ","-").replace(",", " "))
+
+    return e_list
+
+
+def get_employee_rest_day(id):
+    pop_weekday_number = datetime.today().isoweekday()
+    weekday = ""
+
+    if pop_weekday_number == 1:
+        weekday = 'Monday'
+    elif pop_weekday_number == 2:
+        weekday = 'Tuesday'
+    elif pop_weekday_number == 3:
+        weekday = 'Wednesday'
+    elif pop_weekday_number == 4:
+        weekday = 'Thursday'
+    elif pop_weekday_number == 5:
+        weekday = 'Friday'
+    elif pop_weekday_number == 6:
+        weekday = 'Saturday'
+    elif pop_weekday_number == 7:
+        weekday = 'Sunday'
+
+    cursor.execute("SELECT " + weekday + " FROM EmployeeSchedule "
+                   "WHERE EmployeeID='" + str(id) + "'")
+    for row in cursor:
+        time = str(row)
+        return re.sub("[(',)]", '', time).rstrip()
